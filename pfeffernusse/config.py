43a13faee6b7629bf2f927eff859ca67951694f0
jlaura <jlaura@usgs.gov>
2018-08-29 09:14:19 -0700

    Update config.py

--- pfeffernusse/config.py
+++ pfeffernusse/config.py
@@ -3,5 +3,5 @@ Config File
 """
 
 # Directory with metakernals 
-mdis = '/data/spice/mess-e_v_h-spice-6-v1.0/messsp_1000/extras/mk' # Messenger
-mro = '/data/spice/mro-m-spice-6-v1.0/mrosp_1000/extras/mk' # Mars Reconnaissance Orbiter
+mdis = '/scratch/jlaura/spice/mess-e_v_h-spice-6-v1.0/messsp_1000/extras/mk' # Messenger
+mro = '/scratch/jlaura/spice/mro-m-spice-6-v1.0/mrosp_1000/extras/mk' # Mars Reconnaissance Orbiter
